ae466a370be81bf3e84b93bf6728717f6e557d27
dohlee <dohlee.bioinfo@gmail.com>
2023-02-22 10:32:52 +0900

    0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'eve_pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'EVE - Pytorch',
   author = 'Dohoon Lee',
